3ac3034519c18dcb1fc98410aada3bf0b3ba61af
kungtsan <kungtsan@gmail.com>
2015-11-30 20:01:49 +0800

    20151130

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.simpleui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,12 +16,20 @@ public class MainActivity extends AppCompatActivity {
     private EditText inputText;
     private CheckBox hideCheckBox;
 
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         inputText = (EditText)findViewById(R.id.inputText);
-        inputText.setText("1234");
+        //inputText.setText("1234");
+        inputText.setText(sharedPreferences.getString("inputText", ""));
+
         inputText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -37,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void submit(View view){
         String text = inputText.getText().toString();
+        editor.putString("inputText", text);
+        editor.commit();
         if(hideCheckBox.isChecked()){
             text = "**********";
             inputText.setText("***********");
